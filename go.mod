d5ef5a063f8148a07ec88c3490ae2dde3d63f925
Sheldon Hull <sheldonhull@users.noreply.github.com>
2022-02-11 13:56:41 -0600

    refactor(me): use glamour card

--- go.mod
+++ go.mod
@@ -2,14 +2,24 @@ module github.com/sheldonhull/sheldonhull
 
 go 1.17
 
-require github.com/pterm/pterm v0.12.33
+require github.com/charmbracelet/glamour v0.5.0
 
 require (
-	github.com/atomicgo/cursor v0.0.1 // indirect
-	github.com/gookit/color v1.4.2 // indirect
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.17 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.9.0 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	github.com/yuin/goldmark v1.4.4 // indirect
+	github.com/yuin/goldmark-emoji v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- me.go
+++ me.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/pterm/pterm"
+	"github.com/charmbracelet/glamour"
 )
 
 const (
@@ -12,20 +13,61 @@ const (
 )
 
 func main() {
-	pterm.Success.Println("The Epic Personal Go Card Of Sheldon Hull")
-	hs := pterm.NewStyle(pterm.BgLightWhite, pterm.BgBlack, pterm.Bold)
 
-	t, err := pterm.DefaultTable.WithStyle(hs).WithData(pterm.TableData{
-		{"Name", "Sheldon Hull"},
-		{"Website", "sheldonhull.com"},
-		{"Github", "github.com/sheldonhull"},
-		{"Areas Of Focus", "AWS, Go, PowerShell, Databases, Automation"},
-	}).Srender()
-	if err != nil {
-		pterm.Error.Printf("render defaulttable: %v\n", err)
-		os.Exit(exitFail)
-	}
+in := `# The Epic Personal Go Card Of Sheldon Hull
 
-	pterm.DefaultBox.WithTitle("Sheldon Hull").Print(t)
+Hi, I'm Sheldon!
+
+You must love Go too.
+
+My personal slice of the internet is:
+
+- [sheldonhull](https://www.sheldonhull.com)
+
+
+## For My Fellow Devs:
+
+- _Preferred Editor:_ Visual Studio Code, and I do enjoy Jetbrains Goland.
+- _Light or Dark Theme:_ Dark, of course. Currently using Night Owl.
+- _Spaces or Tabs:_ whatever gitattributes defines.
+- Font: Jet Brains Mono & FuraCode Nerd Font Mono (ligature based font)
+- _Source Control:_ Git
+- _Automation:_ Azure DevOps, GitHub Actions (GitLab also seems promising)
+- _Preferred Computer Setup:_ Ultrawide, mechanical keyboard, and latte...
+- _Blog:_ As time permits, sheldonhull.com, though most projects I've done are not public
+- _Terminal:_ Warp (Rust Based) terminal for macOS, VSCode Terminal for almost everything else. I've got a slick zinit zsh setup synced through chezmoi.
+- _If I was an emoji I'd be:_ 🌮🌮🌮
+
+`
+
+r, _ := glamour.NewTermRenderer(
+    // detect background color and pick either the default dark or light theme
+    glamour.WithAutoStyle(),
+    // wrap output at specific width
+    glamour.WithWordWrap(100),
+
+)
+out, err := r.Render(in)
+if err != nil {
+	fmt.Printf("I have failed you and apologize. Quite embarassing. Can I just say, \"Worked on my machine?\"\n")
+os.Exit(1)
+}
+fmt.Print(out)
+
+	// pterm.Success.Println("The Epic Personal Go Card Of Sheldon Hull")
+	// hs := pterm.NewStyle(pterm.BgLightWhite, pterm.BgBlack, pterm.Bold)
+
+	// t, err := pterm.DefaultTable.WithStyle(hs).WithData(pterm.TableData{
+	// 	{"Name", "Sheldon Hull"},
+	// 	{"Website", "sheldonhull.com"},
+	// 	{"Github", "github.com/sheldonhull"},
+	// 	{"Areas Of Focus", "AWS, Go, PowerShell, Databases, Automation"},
+	// }).Srender()
+	// if err != nil {
+	// 	pterm.Error.Printf("render defaulttable: %v\n", err)
+	// 	os.Exit(exitFail)
+	// }
+
+	// pterm.DefaultBox.WithTitle("Sheldon Hull").Print(t)
 	os.Exit(exitSuccess)
 }
